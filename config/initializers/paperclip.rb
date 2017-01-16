6e8991a4ccb88c9797167b037ca26d26aa98f98c
Michael Ray <michaelburtonray@gmail.com>
2017-01-16 12:31:22 -0500

    enable paperclip

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,5 +1,4 @@
-# Paperclip::Attachment.default_options[:storage] = :s3
-# Paperclip::Attachment.default_options[:url] = 's3_domain_url'
-# Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
-# # Paperclip::Attachment.default_options[:styles] = { :large => '2560x1440' }
-# Paperclip::Attachment.default_options[:convert_options] = { :thumb => '-quality 75 -strip', :content => '-quality 75 -strip -interlace Plane'}
+Paperclip::Attachment.default_options[:storage] = :s3
+Paperclip::Attachment.default_options[:url] = 's3_domain_url'
+Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
+Paperclip::Attachment.default_options[:convert_options] = { :thumb => '-quality 75 -strip', :content => '-quality 75 -strip -interlace Plane'}
